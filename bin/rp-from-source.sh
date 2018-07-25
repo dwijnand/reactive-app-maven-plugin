c879f9020598e177d8b6c4e3d6dfdfc008ebc29b
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-25 16:55:39 +0100

    Clone and build reactive-cli only once in 1 place

--- bin/rp-from-source.sh
+++ bin/rp-from-source.sh
@@ -4,6 +4,10 @@ set -o pipefail
 
 die() { echo "Aborting: $*"; exit 1; }
 
-[[ -d reactive-cli ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli
-cd reactive-cli || die "Failed to cd into reactive-cli"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+dir="$DIR/reactive-cli"
+
+[[ -d reactive-cli ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli "$dir"
+cd "$dir" || die "Failed to cd into reactive-cli"
 sbt "cliNative/run $*"
