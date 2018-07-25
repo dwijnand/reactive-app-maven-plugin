db8848448b2f07ea670e6b58f0ba69a1741f9956
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-25 16:03:56 +0100

    checkout tag

--- bin/rp-from-source.sh
+++ bin/rp-from-source.sh
@@ -4,7 +4,6 @@ set -o pipefail
 
 die() { echo "Aborting: $*"; exit 1; }
 
-[[ -d reactive-cli ]] || git clone --depth=1 https://github.com/lightbend/reactive-cli
+[[ -d reactive-cli ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli
 cd reactive-cli || die "Failed to cd into reactive-cli"
-git checkout v1.3.0 || die "Failed to git checkout"
 sbt "cliNative/run $*"
