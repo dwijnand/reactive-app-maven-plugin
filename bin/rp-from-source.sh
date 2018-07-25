53142a72cb80953fefcdf050b53ae5f6ac0eb020
Dale Wijnand <dale.wijnand@gmail.com>
2018-07-25 16:27:54 +0100

    debug sbt call

--- bin/rp-from-source.sh
+++ bin/rp-from-source.sh
@@ -6,4 +6,4 @@ die() { echo "Aborting: $*"; exit 1; }
 
 [[ -d reactive-cli ]] || git clone --depth 1 --branch v1.3.0 https://github.com/lightbend/reactive-cli
 cd reactive-cli || die "Failed to cd into reactive-cli"
-sbt "cliNative/run $*"
+sbt -v "cliNative/run $*"
